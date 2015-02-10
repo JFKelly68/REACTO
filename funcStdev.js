1d8d472759eb5ec6eb4f3fe8a962bf2fa0d0c5da
James Kelly <mrjamesfkelly@gmail.com>
2015-02-10 11:16:26 -0500

    Changed a strange operator

--- funcStdev.js
+++ funcStdev.js
@@ -193,7 +193,7 @@ var compose = function() {
 	var funcs = arguments;
 	return function() {
 		var args = arguments;
-		for (var i = funcs.length; i --> 0;) {
+		for (var i = funcs.length; i-- > 0;) {
 			args = [funcs[i].apply(this, args)];
 		}
 		return args[0];
